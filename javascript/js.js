9d2547e6c263ddeb8d50d008a99908649d967672
hasanisokay <hasanisokay@gmail.com>
2023-02-17 23:02:05 +0600

    added background color change on mouseenter

---
+++ javascript/js.js
@@ -0,0 +1,16 @@
+const cards = document.getElementsByClassName('card');
+for(const card of cards){
+    card.addEventListener("mouseenter",function(event){
+        randomBgColor(card);
+    card.addEventListener("mouseleave",function(){
+        card.style.backgroundColor ='white'
+    })
+})}
+
+function randomBgColor(c){
+    const x = Math.floor(Math.random() * 256);
+    const y = Math.floor(Math.random() * 256);
+    const z = Math.floor(Math.random() * 256);
+    const  bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    c.style.backgroundColor = bgColor;
+}
